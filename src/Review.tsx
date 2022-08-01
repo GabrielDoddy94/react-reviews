f9e9e8473aa66177535ed7076280506b2ef84b87
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-08-01 14:13:25 -0300

    setup the project

---
+++ src/Review.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+import people from "./data";
+
+export function Review() {
+  const [index, setIndex] = useState(0);
+
+  const { name, job, image, text } = people[index];
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+
+      <div className="button-container">
+        <button className="prev-btn">
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn">
+          <FaChevronRight />
+        </button>
+      </div>
+
+      <button className="random-btn">surprise me</button>
+    </article>
+  );
+}
